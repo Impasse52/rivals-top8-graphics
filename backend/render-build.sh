c5df232af0549c857bdec4373454b4f90c2ae020
Impasse52 <giuseppe.termerissa@proton.me>
2023-09-20 21:08:42 +0200

    modified:   backend/render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -19,4 +19,6 @@ fi
 # be sure to add Chromes location to the PATH as part of your Start Command
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
+ls
+
 pip install -r requirements.txt
